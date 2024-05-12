e7abd926ae2cc56ab4a05659e794228675b564e7
azanmassou <azanmassouhappylouis@gmail.com>
2024-05-12 11:11:27 +0200

    More contactMail

--- contactMail.php
+++ contactMail.php
@@ -1,92 +1,51 @@
 <?php
-
-// error_reporting(E_ALL & ~E_DEPRECATED);
-// ini_set("display_errors", 1);
-
-$projet = $_POST['projet'];
-$montant = $_POST['montant'];
-$duree = $_POST['duree'];
-$emprunt = $_POST['emprunt'];
-$pays = $_POST['pays'];
-$adress = $_POST['adress'];
-$postal = $_POST['postal'];
-$ville = $_POST['ville'];
-$situation_logement = $_POST['situation_logement'];
-$situation_professionnelle = $_POST['situation_professionnelle'];
-$revenus = $_POST['revenus'];
-$situation_famillial = $_POST['situation_famillial'];
-$etat_civil = $_POST['etat_civil'];
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$date = $_POST['date'];
-$nation = $_POST['nation'];
-$tel = $_POST['tel'];
-$email = $_POST['email'];
-
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-//Load Composer's autoloader
+// Inclure le fichier d'autoloader de PHPMailer
 require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$objet = $_POST['objet'];
+$tel = $_POST['tel'];
+$message = $_POST['message'];
+$email = $_POST['email'];
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'azanmassouhappylouis@gmail.com';                     //SMTP username
-    $mail->Password   = 'grge pler zqoi jpyr';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom($email, $email);
-    $mail->addAddress('Contacten@finanscokrediet.com', 'finanscokrediet');     //Add a recipient
-    // $mail->addAddress('azanmassouhappylouis@gmail.com');               //Name is optional
-    // $mail->addReplyTo('azanmassouhappylouis@gmail.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+// Créer une nouvelle instance de PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+// Configurer les paramètres du serveur SMTP
+$mail->isSMTP();
+$mail->Host = 'mail.credito-mas-simple.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'support@credito-mas-simple.com';
+$mail->Password = 'Happylouis66'; // Remplacez par votre mot de passe
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Finansco : Soumission du formulaire de Pret';
+// Configurer l'expéditeur et le destinataire
+$mail->setFrom($_POST['email']);
+$mail->addAddress('azanmassouhappylouis@gmail.com');
+// $mail->addAddress('Contacten@finanscokrediet.com'); 
 
-    $mail->Body = "Bonjour,\n\n";
-    $mail->Body .= "Une nouvelle demande de credit a ete soumise depuis votre site :<br>";
-    $mail->Body .= "Projet : $projet\n<br>";
-    $mail->Body .= "Montant : $montant\n<br>";
-    $mail->Body .= "Durer : $duree\n<br>";
-    $mail->Body .= "Emprunt : $emprunt\n<br>";
-    $mail->Body .= "Pays de residence: $pays\n<br>";
-    $mail->Body .= "Adress : $adress\n<br>";
-    $mail->Body .= "Postal : $postal\<br>";
-    $mail->Body .= "Ville : $ville\n<br>";
-    $mail->Body .= "Situation Logement : $situation_logement\n<br>";
-    $mail->Body .= "Situation Professionnelle : $situation_professionnelle\n<br>";
-    $mail->Body .= "Situation Familliale : $situation_famillial\n<br>";
-    $mail->Body .= "Etat Civil : $etat_civil\n<br>";
-    $mail->Body .= "Nom : $nom\n<br>";
-    $mail->Body .= "Date : $date\n<br>";
-    $mail->Body .= "Pays de naissance : $nation\n<br>";
-    $mail->Body .= "Telephone : $tel\n<br>";
-    $mail->Body .= "Email : $email\n<br>";
-    $mail->Body .= "Merci.";
-    $mail->AltBody = 'Une nouvelle demande de crédit a été soumise depuis votre site :\n\n';
-    $mail->send();
-    header('Location:confirmation/index.html');
-    // header("Location: https://finanscokrediet.com/confirmation/index.html");
+// Configurer le contenu de l'e-mail
+$mail->isHTML(true);
+$mail->Subject = 'Finansco : Demande de Contact';
 
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+$mail->Body = "Bonjour,\n\n";
+$mail->Body .= "Une nouvelle demande de Contact a ete soumise depuis votre site :<br>";
+$mail->Body .= "Voornaam : $prenom\n<br>";
+$mail->Body .= "Naam : $nom\n<br>";
+$mail->Body .= "E-mail : $email\n<br>";
+$mail->Body .= "Telefoon : $tel\n<br>";
+$mail->Body .= "Object : $objet\n<br>";
+$mail->Body .= "Bericht : $message\n<br>";
+
+$mail->AltBody = 'Une nouvelle demande demande de contact a été soumise depuis votre site :\n\n';
+
+// Envoyer l'e-mail
+if(!$mail->send()) {
+    echo 'Erreur lors de l\'envoi de l\'e-mail : ' . $mail->ErrorInfo;
+} else {
+    header("Location: confirmation/index.html");
+    echo '0!';
 }
